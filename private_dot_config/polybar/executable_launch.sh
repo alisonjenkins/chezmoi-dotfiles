3372749470d0b1f1b397983636e7baac5961978a
Alan Jenkins <alan.jenkins@uk.clara.net>
2020-02-13 09:27:30 +0000

    Add Setup Polybar for i3 and bspwm

--- private_dot_config/polybar/executable_launch.sh
+++ private_dot_config/polybar/executable_launch.sh
@@ -2,8 +2,14 @@
 
 pkill polybar
 
+CONFIG=config
+
+if [ "$DESKTOP_SESSION" == '/usr/share/xsessions/bspwm' ]; then
+  CONFIG=config-bspwm
+fi
+
 for m in $(polybar --list-monitors | cut -d":" -f1); do
-  MONITOR=$m polybar --reload bar1 &
+  MONITOR=$m polybar --reload bar1 --config "$HOME/.config/polybar/$CONFIG" &
 done
 
 echo "Bars launched..."
