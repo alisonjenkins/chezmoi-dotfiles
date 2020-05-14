71bee00b657cd479256b0839d3957decc73edb38
Alan Jenkins <alan.jenkins@claranet.uk>
2020-05-14 04:44:25 +0000

    Remove duplicate bash aliases section

--- private_dot_config/bashrc.d/aliases.sh
+++ private_dot_config/bashrc.d/aliases.sh
@@ -14,7 +14,6 @@ alias cdgo="cd \$GOPATH"
 # Archlinux specific aliases
 alias makepkg='chrt --idle 0 ionice -c idle makepkg'
 
-
 # {{{ Misc
 alias awsconsole="~/git/awsconsole/awsconsole -b google-chrome-stable"
 alias librarian-puppet='docker run --rm -v "$PWD:/puppet" -v /etc/passwd:/etc/passwd:ro --user="$(id -u):$(id -g)" claranet/librarian-puppet'
@@ -22,13 +21,13 @@ alias codebuild="~/git/aws-codebuild-docker-images/local_builds/codebuild_build.
 alias ewm="cd ~/git/ewmg"
 alias fpm='docker run --rm -v "${WORKSPACE}:/source" -v /etc/passwd:/etc/passwd:ro --user=$(id -u):$(id -g) claranet/fpm'
 alias jctlw="sudo journalctl -u wpa_supplicant@wlp58s0"
-alias key="ssh-add ~/git/ssh_keys/id_bashton_alan"
-alias keyb="ssh-add ~/git/ssh_keys/id_bashton"
+alias key="ssh-add ~/.ssh/ssh_keys/id_bashton_alan"
+alias keyb="ssh-add ~/.ssh/ssh_keys/id_bashton"
 alias keycl="ssh-add -D"
-alias keyaur="ssh-add ~/git/ssh_keys/id_aur"
-alias keyp="ssh-add ~/git/ssh_keys/id_personal"
-alias keypa="ssh-add ~/git/ssh_keys/id_alan-aws"
-alias keypo="ssh-add ~/git/ssh_keys/id_personal_old"
+alias keyaur="ssh-add ~/.ssh/ssh_keys/id_aur"
+alias keyp="ssh-add ~/.ssh/ssh_keys/id_personal"
+alias keypa="ssh-add ~/.ssh/ssh_keys/id_alan-aws"
+alias keypo="ssh-add ~/.ssh/ssh_keys/id_personal_old"
 alias kmse='export EYAML_CONFIG=$PWD/.kms-eyaml.yaml'
 alias misg="cd ~/git/missguided"
 alias sdl="cd ~/git/superdry/laguna"
@@ -58,47 +57,4 @@ else
   alias lt='exa --tree --git --long';
 fi
 
-# {{{ Misc
-alias awsconsole="~/git/awsconsole/awsconsole -b google-chrome-stable"
-alias librarian-puppet='docker run --rm -v "$PWD:/puppet" -v /etc/passwd:/etc/passwd:ro --user="$(id -u):$(id -g)" claranet/librarian-puppet'
-alias codebuild="~/git/aws-codebuild-docker-images/local_builds/codebuild_build.sh"
-alias ewm="cd ~/git/ewmg"
-alias fpm='docker run --rm -v "${WORKSPACE}:/source" -v /etc/passwd:/etc/passwd:ro --user=$(id -u):$(id -g) claranet/fpm'
-alias jctlw="sudo journalctl -u wpa_supplicant@wlp58s0"
-alias key="ssh-add ~/git/ssh_keys/id_bashton_alan"
-alias keyb="ssh-add ~/git/ssh_keys/id_bashton"
-alias keycl="ssh-add -D"
-alias keyaur="ssh-add ~/git/ssh_keys/id_aur"
-alias keyp="ssh-add ~/git/ssh_keys/id_personal"
-alias keypa="ssh-add ~/git/ssh_keys/id_alan-aws"
-alias keypo="ssh-add ~/git/ssh_keys/id_personal_old"
-alias kmse='export EYAML_CONFIG=$PWD/.kms-eyaml.yaml'
-alias misg="cd ~/git/missguided"
-alias rewifi="sudo systemctl restart wpa_supplicant@wlp58s0"
-alias sdl="cd ~/git/superdry/laguna"
-alias sde="cd ~/git/superdry/superdry-everest"
-alias sdm="cd ~/git/superdry/superdry-maverick"
-alias sdo="cd ~/git/superdry/osaka"
-alias sdw="cd ~/git/superdry/windcheater"
-alias sdh="cd ~/git/superdry/superdry-henley"
-# alias sleep="i3lock && sudo systemctl suspend"
-alias snowrep="~/git/bashton-servicenow/reports.py"
-alias snowtick="~/git/bashton-servicenow/view-ticket.py --nobox"
-alias suspend="xscreensaver-command -lock && sleep 1 && sudo systemctl suspend"
-alias tf="terraform"
-alias tw="task +work"
-alias tp="task +personal"
-alias vim="nvim"
-alias vi="nvim"
-alias gpu-on="sudo tee /proc/acpi/bbswitch <<<ON"
-alias gpu-off="sudo tee /proc/acpi/bbswitch <<<OFF"
-
-if uname -a | grep 'Darwin' &> /dev/null; then
-  alias ll='ls -G';
-  alias ls='ls -G';
-else
-  alias ll='exa -l --grid --git';
-  alias ls='exa';
-  alias lt='exa --tree --git --long';
-fi
 # }}}
