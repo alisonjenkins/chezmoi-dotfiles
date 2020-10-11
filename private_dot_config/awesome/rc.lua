0833d0ea884e9d05b86b1a556e0cb5bdb441118b
Alan Jenkins <alan.james.jenkins@gmail.com>
2020-10-11 18:08:25 +0100

    Set the correct screen locking program in Awesome

--- private_dot_config/awesome/rc.lua
+++ private_dot_config/awesome/rc.lua
@@ -95,7 +95,7 @@ local cycle_prev   = true -- cycle trough all previous client or just the first 
 local editor       = os.getenv("EDITOR") or "nvim"
 local gui_editor   = os.getenv("GUI_EDITOR") or "nvim"
 local browser      = os.getenv("BROWSER") or "firefox"
-local scrlocker    = "slock"
+local scrlocker    = "betterlockscreen -l"
 
 awful.util.terminal = terminal
 awful.util.tagnames = { "1", "2", "3", "4", "5" }
