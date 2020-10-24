29acd9adf49b55134977a1aed0cf80156833870e
Alan Jenkins <alan.james.jenkins@gmail.com>
2020-10-25 00:14:22 +0100

    Setup the autostart properly for awesome

--- private_dot_config/awesome/rc.lua
+++ private_dot_config/awesome/rc.lua
@@ -57,18 +57,15 @@ local function run_once(cmd_arr)
     end
 end
 
-run_once({ "urxvtd", "unclutter -root" }) -- entries must be separated by commas
+run_once({ "unclutter -root" }) -- entries must be separated by commas
 
 -- This function implements the XDG autostart specification
---[[
 awful.spawn.with_shell(
     'if (xrdb -query | grep -q "^awesome\\.started:\\s*true$"); then exit; fi;' ..
     'xrdb -merge <<< "awesome.started:true";' ..
     -- list each of your autostart commands, followed by ; inside single quotes, followed by ..
-    'dex --environment Awesome --autostart --search-paths "$XDG_CONFIG_DIRS/autostart:$XDG_CONFIG_HOME/autostart"' -- https://github.com/jceb/dex
+    'dex --environment Awesome --autostart' -- https://github.com/jceb/dex
 )
---]]
-
 -- }}}
 
 -- {{{ Variable definitions
