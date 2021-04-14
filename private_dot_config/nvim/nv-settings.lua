995cc0799c03baf219d4ab09bdf12a75af75bd47
Alan Jenkins <alan.james.jenkins@gmail.com>
2021-04-14 08:01:25 +0100

    Clean up nv-settings

--- private_dot_config/nvim/nv-settings.lua
+++ private_dot_config/nvim/nv-settings.lua
@@ -5,18 +5,15 @@ Formatters and linters should be
 filled in as strings with either
 a global executable or a path to
 an executable
-]] -- general
+]]
+-- general
 O.auto_complete = true
 O.colorscheme = 'gruvbox-material'
 O.auto_close_tree = 0
 O.wrap_lines = false
 
 -- python
--- add things like O.python.formatter.yapf.exec_path
--- add things like O.python.linter.flake8.exec_path
--- add things like O.python.formatter.isort.exec_path
 O.python.formatter = 'yapf'
--- O.python.linter = 'flake8'
 O.python.isort = true
 O.python.autoformat = true
 O.python.diagnostics.virtual_text = true
@@ -25,7 +22,6 @@ O.python.diagnostics.underline = true
 
 -- lua
 O.lua.formatter = 'lua-format'
--- O.lua.formatter = 'lua-format'
 O.lua.autoformat = true
 
 -- javascript
@@ -35,5 +31,3 @@ O.tsserver.autoformat = true
 
 -- json
 O.json.autoformat = true
-
--- create custom autocommand field (This would be easy with lua)
