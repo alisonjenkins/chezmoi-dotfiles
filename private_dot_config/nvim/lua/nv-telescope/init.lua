9c6984a3d8f119e1bf2088afed55381848c98dc2
Alan Jenkins <alan.james.jenkins@gmail.com>
2021-04-15 20:38:28 +0100

    Fix telescope config mistake

--- private_dot_config/nvim/lua/nv-telescope/init.lua
+++ private_dot_config/nvim/lua/nv-telescope/init.lua
@@ -66,11 +66,11 @@ require('telescope').setup {
             -- defaults to {"png", "jpg", "mp4", "webm", "pdf"}
             filetypes = {"png", "webp", "jpg", "jpeg"},
             find_cmd = "rg" -- find command (defaults to `fd`)
-        }
+        },
         fzf = {
             override_generic_sorter = false, -- override the generic sorter
             override_file_sorter = true,     -- override the file sorter
-            case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+            case_mode = "smart_case"        -- or "ignore_case" or "respect_case"
                                              -- the default case_mode is "smart_case"
         }
     }
