f4853ed792a90bcd35c49f6b4b70c2cb2aff8e16
Alan Jenkins <alan.james.jenkins@gmail.com>
2021-04-15 20:44:15 +0100

    Fix missing invocation of setup_servers in lspinstall config

--- private_dot_config/nvim/lua/nv-lspinstall/init.lua
+++ private_dot_config/nvim/lua/nv-lspinstall/init.lua
@@ -6,6 +6,8 @@ local function setup_servers()
   end
 end
 
+setup_servers()
+
 -- Automatically reload after `:LspInstall <server>` so we don't have to restart neovim
 require'lspinstall'.post_install_hook = function ()
   setup_servers()    -- reload installed servers
