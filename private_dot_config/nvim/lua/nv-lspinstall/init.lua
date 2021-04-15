4e95fa6635869ec4ccffa9f72253962567b5cc52
Alan Jenkins <alan.james.jenkins@gmail.com>
2021-04-15 20:43:00 +0100

    Fix indentation of lspinstall config

--- private_dot_config/nvim/lua/nv-lspinstall/init.lua
+++ private_dot_config/nvim/lua/nv-lspinstall/init.lua
@@ -1,9 +1,9 @@
 local function setup_servers()
   require'lspinstall'.setup()
   local servers = require'lspinstall'.installed_servers()
-   for _, server in pairs(servers) do
-     require'lspconfig'[server].setup{}
-   end
+  for _, server in pairs(servers) do
+    require'lspconfig'[server].setup{}
+  end
 end
 
 -- Automatically reload after `:LspInstall <server>` so we don't have to restart neovim
