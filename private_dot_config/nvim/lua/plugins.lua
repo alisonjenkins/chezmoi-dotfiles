a061755171e29142beb3c02db413d1f40572877f
Alan Jenkins <alan.james.jenkins@gmail.com>
2021-04-30 07:51:51 +0100

    Add Vim Vinegar

--- private_dot_config/nvim/lua/plugins.lua
+++ private_dot_config/nvim/lua/plugins.lua
@@ -77,6 +77,7 @@ return require('packer').startup(function(use)
     use 'tpope/vim-rhubarb'                                                                          -- (vimscript) Adds Fugitive Gbrowse support for GitHub repos.
     use 'tpope/vim-sensible'                                                                         -- Sensible default options for Neovim
     use 'tpope/vim-unimpaired'                                                                       -- (vimscript) Adds many common sense mappings for working with buffers, quickfix, lines, SCM conflict markers, etc
+    use 'tpope/vim-vinegar'                                                                          -- (vimscript) Adds a shortcut (-) for opening Netrw for browsing files.
     use 'turbio/bracey.vim'                                                                          -- (vimscript) A plugin for live HTML, Javascript and CSS editing.
     use 'unblevable/quick-scope'                                                                     -- (vimscript) Provides an overlay when using the 'f', 'F', 't' and 'T' motions to help with jumping to characters.
     use 'vim-scripts/Align'                                                                          -- (vimscript) Allows aligning sections of text (for example these comments in this plugin file (use visual to select the text and do :Align -- <CR>))
