4d811088e587beb8db285ea212f5c3ec981d538f
Alan Jenkins <a.jenkins@synalogik.com>
2021-04-30 15:38:59 +0100

    Properly Configure Firenvim

--- private_dot_config/nvim/lua/nv-firenvim/init.lua
+++ private_dot_config/nvim/lua/nv-firenvim/init.lua
@@ -1,5 +1,11 @@
 vim.api.nvim_exec([[
+let g:firenvim_config = { 'globalSettings': { 'alt': 'all' }, 'localSettings': { '.*': { 'cmdline': 'neovim', 'content': 'text', 'priority': 0, 'selector': 'textarea', 'takeover': 'never', } } }
+
+let fc = g:firenvim_config['localSettings']
+
 if exists('g:started_by_firenvim')
   set guifont=Monospace\ 8
 endif
+
+au BufEnter github.com_*_TEXTAREA-id-pull-request-body.txt set filetype=markdown
 ]], true)
