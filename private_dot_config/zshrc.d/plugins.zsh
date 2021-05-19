5569cc75ddb87db15d894473035855339a6cdcef
Alan Jenkins <a.jenkins@synalogik.com>
2021-05-19 10:54:38 +0100

    Clean up ZSH plugins

--- private_dot_config/zshrc.d/plugins.zsh
+++ private_dot_config/zshrc.d/plugins.zsh
@@ -8,12 +8,6 @@ ZINIT[HOME_DIR]=~/.local/share/zinit
 
 source ~/.local/share/zinit/bin/zinit.zsh
 
-# Install tools used by plugins
-# export PISTOL_CHROMA_STYLE="native"
-# zinit as"program" make'!' \
-#     atpull'%atclone' pick"pistol" for \
-#         doronbehar/pistol
-
 export _ZL_MATCH_MODE=1
 zinit as"program"  \
     atpull'%atclone' pick"z.lua" src"z.lua.plugin.zsh" for \
@@ -27,10 +21,8 @@ zinit from"gh-r" as"program" mv"direnv* -> direnv" \
 
 # Plugins
 zinit load Aloxaf/fzf-tab
-# zinit load alanjjenkins/assume-role-1
-# zinit load alanjjenkins/zsh-my-aws
 zinit load chriskempson/base16-shell
-# zinit load fabiokiatkowski/exercism.plugin.zsh
+zinit load fabiokiatkowski/exercism.plugin.zsh
 zinit load joepvd/zsh-hints
 
 zinit load alanjjenkins/asdf.plugin.zsh
@@ -42,9 +34,6 @@ zinit load zsh-users/zsh-completions
 zinit wait lucid for \
   Dbz/kube-aliases
 
-# zinit as"completion" mv"lf.zsh -> _lf" for 'https://raw.githubusercontent.com/gokcehan/lf/master/etc/lf.zsh'
-# zinit snippet 'https://raw.githubusercontent.com/gokcehan/lf/master/etc/lfcd.sh'
-
 zinit snippet 'https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/plugins/aws/aws.plugin.zsh'
 zinit snippet 'https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/plugins/command-not-found/command-not-found.plugin.zsh'
 
@@ -52,30 +41,8 @@ zinit snippet 'https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/plugins/
 zinit ice lucid wait
 zinit snippet OMZP::fzf
 
-# zinit ice wait
-# zinit light unixorn/kubectx-zshplugin
-
-# zinit ice lucid wait
-# zinit load softmoth/zsh-vim-mode
-
-# zinit ice lucid wait
-# zinit load Dbz/kube-aliases
+zinit ice lucid wait
+zinit load softmoth/zsh-vim-mode
 
 autoload -Uz _zinit
 (( ${+_comps} )) && _comps[zinit]=_zinit
-
-# Kubectl completion
-# source <(kubectl completion zsh)
-# complete -F __start_kubectl k
-
-# Helm completion
-# source <(helm completion zsh)
-
-# fluxctl completion
-# source <(fluxctl completion zsh)
-
-# Kubens and kubectx completion
-# if [ -d ~/.asdf/installs/kubectx/0.9.0/completion ]; then
-#   source ~/.asdf/installs/kubectx/0.9.0/completion/kubectx.zsh
-  # source ~/.asdf/installs/kubectx/0.9.0/completion/kubens.zsh
-# fi
