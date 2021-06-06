a694b7a2093ed8ca7aef85f2eb788c16c092d20d
Alan Jenkins <alan.james.jenkins@gmail.com>
2021-06-06 19:00:59 +0100

    Improve sumneko_lua backup config

--- private_dot_config/nvim/lua/lspconfig-config/sumneko_lua.lua
+++ private_dot_config/nvim/lua/lspconfig-config/sumneko_lua.lua
@@ -23,5 +23,31 @@ if hasluadev then
     })
     lspconfig.sumneko_lua.setup(luadev)
 else
-    lspconfig.sumneko_lua.setup(lua_server)
+    local runtime_path = vim.split(package.path, ';')
+
+    lspconfig.sumneko_lua.setup{
+        cmd=lua_server,
+        settings = {
+            Lua = {
+                runtime = {
+                    -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+                    version = 'LuaJIT',
+                    -- Setup your lua path
+                    path = runtime_path,
+                },
+                diagnostics = {
+                    -- Get the language server to recognize the `vim` global
+                    globals = {'vim'},
+                },
+                workspace = {
+                    -- Make the server aware of Neovim runtime files
+                    library = vim.api.nvim_get_runtime_file("", true),
+                },
+                -- Do not send telemetry data containing a randomized but unique identifier
+                telemetry = {
+                    enable = false,
+                },
+            },
+        },
+    }
 end
