1be3f1539e2212bd28ac5323ee79bd3d0eefa647
Alan Jenkins <alan.james.jenkins@gmail.com>
2021-06-07 07:28:45 +0100

    Add configs for various languages for efm-langserver.

--- private_dot_config/nvim/lua/lspconfig-config/efm.lua
+++ private_dot_config/nvim/lua/lspconfig-config/efm.lua
@@ -13,9 +13,39 @@ lspconfig.efm.setup{
     settings = {
         rootMarkers = {".git/"},
         languages = {
+            css = {
+                { formatCommand = "prettier --stdin-filepath ${INPUT}", formatStdin = true }
+            },
+            html = {
+                { formatCommand = "prettier --stdin-filepath ${INPUT}", formatStdin = true }
+            },
+            javascript = {
+                { lintCommand = "eslint_d -f unix --stdin --stdin-filename ${INPUT}", lintIgnoreExitCode = true, lintStdin = true, lintFormats = {"%f:%l:%c: %m"}, },
+                { formatCommand = "prettier --stdin-filepath ${INPUT}", formatStdin = true },
+            },
+            javascriptreact = {
+                { formatCommand = "prettier --stdin-filepath ${INPUT}", formatStdin = true },
+                { lintCommand = "eslint_d -f unix --stdin --stdin-filename ${INPUT}", lintIgnoreExitCode = true, lintStdin = true, lintFormats = {"%f:%l:%c: %m"}, },
+            },
+            json = {
+                { lintCommand = "jq .", formatStdin = true },
+                { formatCommand = "prettier --stdin-filepath ${INPUT}", formatStdin = true }
+            },
             lua = {
-                {formatCommand = "lua-format -i", formatStdin = true}
-            }
+                { formatCommand = "lua-format -i", formatStdin = true },
+            },
+            markdown = {
+                { formatCommand = "prettier --stdin-filepath ${INPUT}", formatStdin = true }
+            },
+            rust = {
+                { formatCommand = "rustfmt", formatStdin = true },
+            },
+            scss = {
+                { formatCommand = "prettier --stdin-filepath ${INPUT}", formatStdin = true }
+            },
+            yaml = {
+                { formatCommand = "prettier --stdin-filepath ${INPUT}", formatStdin = true }
+            },
         }
     }
 }
