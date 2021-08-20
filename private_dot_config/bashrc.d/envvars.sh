c0e662c12cf9713cb9b7035d03f298cce0281d02
Alan Jenkins <alan.james.jenkins@gmail.com>
2021-08-20 08:23:21 +0100

    Add AWS Vault backend envvar for Linux

--- private_dot_config/bashrc.d/envvars.sh
+++ private_dot_config/bashrc.d/envvars.sh
@@ -24,3 +24,7 @@ if [ -z "$SSH_AUTH_SOCK" ];
 then
     export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent.socket"
 fi
+
+if uname -a | grep 'Linux' &> /dev/null; then
+  export AWS_VAULT_BACKEND="file"
+fi
