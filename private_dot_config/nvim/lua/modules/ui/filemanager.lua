7b548e200e8a4fa384e8fab6ca56bfc61351067b
Alan Jenkins <alan.james.jenkins@gmail.com>
2021-11-04 08:48:24 +0000

    Neovim config refactor
    Modelling the config after the impressive organisation of
    https://github.com/mnabila/nvimrc.

---
+++ private_dot_config/nvim/lua/modules/ui/filemanager.lua
@@ -0,0 +1,85 @@
+local M = {}
+
+function M.oldconfig()
+    vim.g.nvim_tree_side = "left"
+    vim.g.nvim_tree_width = 35
+    vim.g.nvim_tree_ignore = { ".git", "node_modules", ".cache" }
+    vim.g.nvim_tree_gitignore = 0
+    vim.g.nvim_tree_auto_open = 0
+    vim.g.nvim_tree_auto_close = 0
+    vim.g.nvim_tree_auto_ignore_ft = {}
+    vim.g.nvim_tree_quit_on_open = 0
+    vim.g.nvim_tree_follow = 1
+    vim.g.nvim_tree_indent_markers = 1
+    vim.g.nvim_tree_hide_dotfiles = 1
+    vim.g.nvim_tree_git_hl = 1
+    vim.g.nvim_tree_highlight_opened_files = 1
+    vim.g.nvim_tree_root_folder_modifier = ":~"
+    vim.g.nvim_tree_tab_open = 1
+    vim.g.nvim_tree_width_allow_resize = 1
+    vim.g.nvim_tree_group_empty = 0
+    vim.g.nvim_tree_disable_window_picker = 1
+    vim.g.nvim_tree_lsp_diagnostics = 0
+    vim.g.nvim_tree_hijack_cursor = 0
+    vim.g.nvim_tree_show_icons = { git = 0, folders = 1, files = 1 }
+
+    vim.g.nvim_tree_icons = {
+        default = "",
+        symlink = "",
+        git = { unstaged = "✗", staged = "✓", unmerged = "", renamed = "➜", untracked = "★" },
+        folder = { default = " ", open = " " },
+    }
+
+    -- disable word wrap
+    vim.cmd("autocmd FileType LuaTree setlocal nowrap")
+end
+
+function M.config()
+    local hasnvimtree, nvimtree = pcall(require, "nvim-tree")
+    if not hasnvimtree then
+        return
+    end
+
+    vim.g.nvim_tree_respect_buf_cwd = 1
+    nvimtree.setup({
+        disable_netrw = true,
+        hijack_netrw = true,
+        open_on_setup = false,
+        ignore_ft_on_setup = {},
+        auto_close = false,
+        open_on_tab = false,
+        update_to_buf_dir = {
+            enable = true,
+            auto_open = true,
+        },
+        hijack_cursor = false,
+        update_cwd = true,
+        diagnostics = {
+            enable = false,
+        },
+        update_focused_file = {
+            enable = true,
+            update_cwd = true,
+            ignore_list = {},
+        },
+        system_open = {
+            cmd = nil,
+            args = {},
+        },
+        view = {
+            width = 35,
+            side = "left",
+            auto_resize = false,
+            mappings = {
+                custom_only = false,
+                list = {},
+            },
+        },
+    })
+end
+
+return setmetatable({}, {
+    __call = function()
+        return M.config()
+    end,
+})
---
+++ private_dot_config/nvim/lua/modules/ui/init.lua
@@ -0,0 +1,90 @@
+local Ui = {}
+
+function Ui.bufferline()
+    return {
+        "romgrk/barbar.nvim",
+        wants = "nvim-web-devicons",
+        requires = { "kyazdani42/nvim-web-devicons", opt = true },
+        config = require("modules.ui.bufferline")(),
+    }
+end
+
+function Ui.statusline()
+    return {
+        "glepnir/galaxyline.nvim",
+        wants = "nvim-web-devicons",
+        requires = { "kyazdani42/nvim-web-devicons", opt = true },
+        config = require("modules.ui.statusline")(),
+    }
+end
+
+function Ui.indentline()
+    return {
+        "lukas-reineke/indent-blankline.nvim",
+        event = "BufRead",
+        setup = function()
+            require("modules.ui.indentline")()
+        end,
+    }
+end
+
+function Ui.filemanager()
+    return {
+        "kyazdani42/nvim-tree.lua",
+        wants = "nvim-web-devicons",
+        requires = { "kyazdani42/nvim-web-devicons", opt = true },
+        config = require("modules.ui.filemanager")(),
+    }
+end
+
+function Ui.telescope()
+    return {
+        "nvim-telescope/telescope.nvim",
+        wants = "nvim-web-devicons",
+        requires = {
+            "nvim-lua/popup.nvim",
+            "nvim-lua/plenary.nvim",
+            "nvim-telescope/telescope-fzy-native.nvim",
+            {
+                "kyazdani42/nvim-web-devicons",
+                opt = true,
+            },
+        },
+        config = require("modules.ui.telescope")(),
+    }
+end
+
+function Ui.treesitter()
+    return {
+
+        "nvim-treesitter/nvim-treesitter",
+        run = ":TSUpdate",
+        wants = {
+            "nvim-ts-rainbow",
+            "nvim-ts-autotag",
+        },
+        requires = {
+            { "p00f/nvim-ts-rainbow", opt = true },
+            { "windwp/nvim-ts-autotag", opt = true },
+        },
+        config = require("modules.ui.treesitter")(),
+    }
+end
+
+function Ui.colorizer()
+    return {
+        "norcalli/nvim-colorizer.lua",
+        config = require("modules.ui.colorizer")(),
+    }
+end
+
+function Ui.colorscheme()
+    -- note: if you want use gruvboy.nvim please change to mnabila/gruvboy.nvim
+    return {
+        "mnabila/gruvboy.nvim",
+--        wants = "gruvboy.nvim",
+--        requires = { "", opt = true },
+    }
+end
+
+return Ui
