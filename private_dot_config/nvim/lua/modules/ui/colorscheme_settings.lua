d719602c73848872e9e49d9e229bbb1d7cf4fb12
Alan Jenkins <alan.james.jenkins@gmail.com>
2021-11-07 15:05:59 +0000

    Rework colorscheme, keymaps and add fugitive!

---
+++ private_dot_config/nvim/lua/modules/ui/colorscheme_settings.lua
@@ -0,0 +1,21 @@
+local M = {}
+
+function M.config()
+    vim.g.tokyonight_style = "night"
+    vim.g.tokyonight_italic_functions = true
+    vim.g.tokyonight_sidebars = { "qf", "vista_kind", "terminal", "packer" }
+
+    vim.cmd([[
+    try
+        colorscheme tokyonight
+    catch
+        colorscheme desert
+    endtry
+    ]])
+end
+
+return setmetatable({}, {
+    __call = function()
+        return M.config()
+    end,
+})
