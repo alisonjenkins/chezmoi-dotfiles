44260327ab4282d3254a044a7f916613eeea83cd
Alan Jenkins <alan.james.jenkins@gmail.com>
2021-11-08 20:40:49 +0000

    Configure the vim-terraform plugin

--- private_dot_config/nvim/lua/modules/editor/init.lua
+++ private_dot_config/nvim/lua/modules/editor/init.lua
@@ -52,7 +52,11 @@ end
 
 function Editor.terraform()
        return {
-               'hashivim/vim-terraform'
+               'hashivim/vim-terraform',
+               config = function()
+                      vim.g.terraform_align = 1
+                      vim.g.terraform_fmt_on_save = 1
+               end
        }
 end
 
