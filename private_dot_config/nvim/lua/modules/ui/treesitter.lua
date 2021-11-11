60ca9d7490eca8973a5e47dc15628e7efa3122a9
Alan Jenkins <a.jenkins@synalogik.com>
2021-11-11 09:09:40 +0000

    Ignore Haskell Treesitter plugin on Mac OS
    It fails to build at the moment.

--- private_dot_config/nvim/lua/modules/ui/treesitter.lua
+++ private_dot_config/nvim/lua/modules/ui/treesitter.lua
@@ -6,8 +6,16 @@ function M.config()
                 return
         end
 
+        local ignore_plugin = {}
+
+        if vim.fn.has("macunix") == 1 then
+                table.insert(ignore_plugin, "haskell")
+        end
+
         local options = {
                 ensure_installed = "all",
+                ignore_install = ignore_plugin,
+
                 highlight = {
                         enable = true,
                         use_languagetree = true,
