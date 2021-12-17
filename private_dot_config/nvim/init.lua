c5ba5825423dac0c5ec7bd221671b36b82eeabd0
Alan Jenkins <a.jenkins@synalogik.com>
2021-12-17 23:54:37 +0000

    Enable lua cache

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -1,8 +1,8 @@
 -- Caching
---local hasimpatient, impatient = pcall(require, "impatient")
---if hasimpatient then
-  --impatient.enable_profile()
---end
+local hasimpatient, impatient = pcall(require, "impatient")
+if hasimpatient then
+  impatient.enable_profile()
+end
 
 require"plugins"
 require"options"
