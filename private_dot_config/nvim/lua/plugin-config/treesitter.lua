7fa56d2f6e579ba39f9f1712062df80ea151dc3d
Alan Jenkins <a.jenkins@synalogik.com>
2021-12-22 18:57:15 +0000

    Further improvements

--- private_dot_config/nvim/lua/plugin-config/treesitter.lua
+++ private_dot_config/nvim/lua/plugin-config/treesitter.lua
@@ -3,63 +3,63 @@ local treesitter = require("nvim-treesitter.configs")
 local ignore_plugin = {}
 
 if vim.fn.has("macunix") == 1 then
-        table.insert(ignore_plugin, "haskell")
+	table.insert(ignore_plugin, "haskell")
 end
 
 local options = {
-        ensure_installed = "all",
-        ignore_install = ignore_plugin,
+	ensure_installed = "all",
+	ignore_install = ignore_plugin,
 
-        highlight = {
-                enable = true,
-                use_languagetree = true,
-        },
-        indent = {
-                enable = false,
-                disable = { "python" },
-        },
-        rainbow = {
-                enable = true,
-        },
-        autopairs = {
-                enable = false,
-        },
-        autotag = {
-                enable = true,
-                filetypes = { "html", "xml" },
-        },
-        context_commentstring = {
-                enable = true,
-                config = {
-                        javascriptreact = {
-                                style_element = '{/*%s*/}'
-                        }
-                }
-        },
-        textobjects = {
-                select = {
-                        enable = true,
+	highlight = {
+		enable = true,
+		use_languagetree = true,
+	},
+	indent = {
+		enable = true,
+		disable = { "python" },
+	},
+	rainbow = {
+		enable = true,
+	},
+	autopairs = {
+		enable = false,
+	},
+	autotag = {
+		enable = true,
+		filetypes = { "html", "xml" },
+	},
+	context_commentstring = {
+		enable = true,
+		config = {
+			javascriptreact = {
+				style_element = "{/*%s*/}",
+			},
+		},
+	},
+	textobjects = {
+		select = {
+			enable = true,
 
-                        -- Automatically jump forward to textobj, similar to targets.vim 
-                        lookahead = true,
+			-- Automatically jump forward to textobj, similar to targets.vim
+			lookahead = true,
 
-                        keymaps = {
-                                -- You can use the capture groups defined in textobjects.scm
-                                ["af"] = "@function.outer",
-                                ["if"] = "@function.inner",
-                                ["ac"] = "@class.outer",
-                                ["ic"] = "@class.inner",
+			keymaps = {
+				-- You can use the capture groups defined in textobjects.scm
+				["af"] = "@function.outer",
+				["if"] = "@function.inner",
+				["ac"] = "@class.outer",
+				["ic"] = "@class.inner",
 
-                                -- Or you can define your own textobjects like this
-                                ["iF"] = {
-                                        python = "(function_definition) @function",
-                                        cpp = "(function_definition) @function",
-                                        c = "(function_definition) @function",
-                                        java = "(method_declaration) @function",
-                                },
-                        },
-                },
-        },
+				-- Or you can define your own textobjects like this
+				["iF"] = {
+					python = "(function_definition) @function",
+					cpp = "(function_definition) @function",
+					c = "(function_definition) @function",
+					java = "(method_declaration) @function",
+				},
+			},
+		},
+	},
 }
 
 treesitter.setup(options)
--- private_dot_config/nvim/lua/plugin-config/trouble.lua
+++ private_dot_config/nvim/lua/plugin-config/trouble.lua
@@ -1,35 +1,35 @@
 local options = {
-        auto_close = false, -- automatically close the list when you have no diagnostics
-        auto_open = false, -- automatically open the list when you have diagnostics
-        auto_preview = true, -- automatically preview the location of the diagnostic. <esc> to close preview and go back
-        fold_closed = "", -- icon used for closed folds
-        fold_open = "", -- icon used for open folds
-        height = 7, -- height of the trouble list
-        icons = true, -- use dev-icons for filenames
-        indent_lines = true, -- add an indent guide below the fold icons
-        mode = "document_diagnostics", -- "workspace" or "document"
-        use_lsp_diagnostic_signs = false, -- enabling this will use the signs defined in your lsp client
+	auto_close = false, -- automatically close the list when you have no diagnostics
+	auto_open = false, -- automatically open the list when you have diagnostics
+	auto_preview = true, -- automatically preview the location of the diagnostic. <esc> to close preview and go back
+	fold_closed = "", -- icon used for closed folds
+	fold_open = "", -- icon used for open folds
+	height = 7, -- height of the trouble list
+	icons = true, -- use dev-icons for filenames
+	indent_lines = true, -- add an indent guide below the fold icons
+	mode = "document_diagnostics", -- "workspace" or "document"
+	use_lsp_diagnostic_signs = false, -- enabling this will use the signs defined in your lsp client
 
-        action_keys = { -- key mappings for actions in the trouble list
-                close = "<Leader>d", -- close the list
-                refresh = "r", -- manually refresh
-                jump = "<cr>", -- jump to the diagnostic or open / close folds
-                toggle_mode = "m", -- toggle between "workspace" and "document" mode
-                toggle_preview = "P", -- toggle auto_preview
-                preview = "p", -- preview the diagnostic location
-                close_folds = "zM", -- close all folds
-                cancel = "<esc>", -- cancel the preview and get back to your last window / buffer / cursor
-                open_folds = "zR", -- open all folds
-                previous = "k", -- preview item
-                next = "j", -- next item
-        },
-        signs = {
-                -- icons / text used for a diagnostic
-                error = "",
-                warning = "",
-                hint = "",
-                information = "",
-        },
+	action_keys = { -- key mappings for actions in the trouble list
+		close = "<Leader>d", -- close the list
+		refresh = "r", -- manually refresh
+		jump = "<cr>", -- jump to the diagnostic or open / close folds
+		toggle_mode = "m", -- toggle between "workspace" and "document" mode
+		toggle_preview = "P", -- toggle auto_preview
+		preview = "p", -- preview the diagnostic location
+		close_folds = "zM", -- close all folds
+		cancel = "<esc>", -- cancel the preview and get back to your last window / buffer / cursor
+		open_folds = "zR", -- open all folds
+		previous = "k", -- preview item
+		next = "j", -- next item
+	},
+	signs = {
+		-- icons / text used for a diagnostic
+		error = "",
+		warning = "",
+		hint = "",
+		information = "",
+	},
 }
 
 local trouble = require("trouble")
