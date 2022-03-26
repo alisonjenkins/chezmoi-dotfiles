751aa3fb884816e653fa72d94161a37019269723
Alan Jenkins <a.jenkins@synalogik.com>
2022-03-26 08:43:21 +0000

    Setup tabline to always show + Tabs in sessions

--- private_dot_config/nvim/lua/options.lua
+++ private_dot_config/nvim/lua/options.lua
@@ -15,6 +15,7 @@ vim.cmd("set completeopt=menu,menuone,noselect") -- Completeopt setting recommen
 vim.cmd("set conceallevel=0") -- So that I can see `` in markdown files
 vim.cmd("set cursorline") -- Enable highlighting of the current line
 vim.cmd("set expandtab") -- Converts tabs to spaces
+vim.cmd("set guioptions-=e") -- Use showtabline in gui vim
 vim.cmd("set hidden") -- Required to keep multiple buffers open multiple buffers
 vim.cmd("set ignorecase") -- ignore case makes searching case insensitive by default. Overridable by using a capital letter thanks to smart case.
 vim.cmd("set laststatus=3") -- only show one shared statusline
@@ -23,6 +24,7 @@ vim.cmd("set nowrap") -- Display long lines as just one line
 vim.cmd("set number") -- set numbered lines
 vim.cmd("set pumheight=10") -- Makes popup menu smaller
 vim.cmd("set relativenumber") -- set relative numbered lines
+vim.cmd("set sessionoptions+=tabpages,globals") -- store tabpages and globals in session
 vim.cmd("set showtabline=2") -- Always show tabs
 vim.cmd("set signcolumn=yes") -- Always show the signcolumn, otherwise it would shift the text each time
 vim.cmd("set smartcase") -- smartcase makes it so that searching becomes case sensitive if you use a capital letter in the search.
