ff9b11c0d199223d91a961fa41ee260ad0d6f5ff
Alan Jenkins <a.jenkins@synalogik.com>
2022-04-02 10:00:50 +0100

    Add code to disable 'naughty' awesome's notifications

---
+++ private_dot_config/awesome/naughty.lua
@@ -0,0 +1,23 @@
+--- Ugly hack to disable Awesome native notifications support
+--
+-- This module shadows builtin naughty module, to prevent it from hijacking
+-- notification popups on D-Bus, so that an external notification daemon can
+-- display them instead. The module also re-implements just enough naughty API
+-- to handle notifications issued from rc.lua, and forward them to the bus.
+--
+-- See also: https://github.com/awesomeWM/awesome/issues/1285
+
+local awful = require("awful")
+
+local naughty = {}
+
+naughty.config = { presets = { critical = "critical" } }
+
+function naughty.notify(args)
+    local urgency = naughty.config.presets[args["preset"]]
+    local summary = args["title"]
+    local body = args["text"]
+    awful.spawn(string.format("notify-send -u %s \"%s\" \"%s\"", urgency, summary, body))
+end
+
+return naughty
