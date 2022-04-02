8de67439229e154bbe23b5674cfe47adc1bde29a
Alan Jenkins <a.jenkins@synalogik.com>
2022-04-02 10:07:27 +0100

    Format awesome configs with stylua

--- private_dot_config/awesome/naughty.lua
+++ private_dot_config/awesome/naughty.lua
@@ -14,10 +14,10 @@ local naughty = {}
 naughty.config = { presets = { critical = "critical" } }
 
 function naughty.notify(args)
-    local urgency = naughty.config.presets[args["preset"]]
-    local summary = args["title"]
-    local body = args["text"]
-    awful.spawn(string.format("notify-send -u %s \"%s\" \"%s\"", urgency, summary, body))
+	local urgency = naughty.config.presets[args["preset"]]
+	local summary = args["title"]
+	local body = args["text"]
+	awful.spawn(string.format('notify-send -u %s "%s" "%s"', urgency, summary, body))
 end
 
 return naughty
