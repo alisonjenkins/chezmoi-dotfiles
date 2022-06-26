c0044635f79e951907e20354ce615b22b7f31b6b
Alan Jenkins <a.jenkins@synalogik.com>
2022-06-26 13:32:20 +0100

    Fix deprecated style options in Kanagawa config

--- private_dot_config/nvim/lua/plugin-config/kanagawa.lua
+++ private_dot_config/nvim/lua/plugin-config/kanagawa.lua
@@ -1,11 +1,9 @@
 require('kanagawa').setup({
     undercurl = true,           -- enable undercurls
-    commentStyle = "italic",
-    functionStyle = "NONE",
-    keywordStyle = "italic",
-    statementStyle = "bold",
-    typeStyle = "NONE",
-    variablebuiltinStyle = "italic",
+    commentStyle = { italic = true },
+    keywordStyle = { italic = true },
+    statementStyle = { bold = true },
+    variablebuiltinStyle = { italic = true },
     specialReturn = true,       -- special highlight for the return keyword
     specialException = true,    -- special highlight for exception handling keywords
     transparent = false,        -- do not set background color
