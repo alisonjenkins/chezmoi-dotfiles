b1d773034cfbf4ae3338a5c2ecf13acb0ff81cab
Alan Jenkins <a.jenkins@synalogik.com>
2022-07-20 20:50:25 +0100

    Install null-ls
    Install null-ls for using non LSP tools and linters as LSPs.

---
+++ private_dot_config/nvim/lua/plugin-config/null-ls.lua
@@ -0,0 +1,33 @@
+local null_ls = require("null-ls")
+local lSsources = {
+  null_ls.builtins.formatting.prettierd.with({
+    filetypes = {
+      "javascript",
+      "typescript",
+      "css",
+      "scss",
+      "html",
+      "json",
+      "yaml",
+      "markdown",
+      "graphql",
+      "md",
+      "txt",
+    },
+  }),
+  null_ls.builtins.formatting.stylua.with({
+    filetypes = {
+      "lua",
+    },
+    args = { "--indent-width", "2", "--indent-type", "Spaces", "-" },
+  }),
+}
+
+null_ls.setup({
+  sources = lSsources,
+  on_attach = function(client, _)
+    if client.supports_method("textDocument/formatting") then
+      require("lsp-format").on_attach(client)
+    end
+  end,
+})
